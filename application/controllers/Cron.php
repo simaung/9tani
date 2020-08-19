48497589cd03382dca9dea1e70c7cde254854364
simaung <simaungproject@gmail.com>
2020-08-19 11:09:49 +0700

    fix process payment transfer for topup deposit

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -41,73 +41,93 @@ class Cron extends CI_Controller
                 if ($get_mutasi->rest_no == 0) {
                     $this->order_model->set_order_paid($transaction_invoice, json_encode($get_mutasi->mutasi_data));
 
-                    $get_transaction = $this->conn['main']
-                        ->select('a.*, b.id as transaction_id, b.merchant_id, c.email, sum(d.price) as total_price, b.shipping_cost, e.description')
-                        ->join('mall_transaction b', 'a.id = b.order_id', 'left')
-                        ->join('user_partner c', 'a.user_id = c.partner_id', 'left')
-                        ->join('mall_transaction_item d', 'b.id = d.transaction_id', 'left')
-                        ->join('payment_channel e', 'a.payment_channel_id = e.id', 'left')
-                        ->where('invoice_code', $transaction_invoice)
-                        ->get('mall_order a')->row();
+                    if (substr($transaction_invoice, 0, 2) == 'ST') {
+                        $get_transaction = $this->conn['main']
+                            ->select('a.*, b.id as transaction_id, b.merchant_id, c.email, sum(d.price) as total_price, b.shipping_cost, e.description')
+                            ->join('mall_transaction b', 'a.id = b.order_id', 'left')
+                            ->join('user_partner c', 'a.user_id = c.partner_id', 'left')
+                            ->join('mall_transaction_item d', 'b.id = d.transaction_id', 'left')
+                            ->join('payment_channel e', 'a.payment_channel_id = e.id', 'left')
+                            ->where('invoice_code', $transaction_invoice)
+                            ->group_by('a.id, b.id')
+                            ->get('mall_order a')->row();
+                    } else {
+                        $get_transaction = $this->conn['main']
+                            ->select('a.*, b.id as transaction_id, c.email, c.full_name')
+                            ->join('user_partner c', 'a.user_id = c.partner_id', 'left')
+                            ->where('invoice_code', $transaction_invoice)
+                            ->get('deposit_topup a')->row();
 
-                    $product = $this->conn['main']
-                        ->select('a.*')
-                        ->select("SHA1(CONCAT(variant_id, '" . $this->config->item('encryption_key') . "')) as variant_id")
-                        ->where('transaction_id', $get_transaction->transaction_id)
-                        ->get('mall_transaction_item a')->result_array();
+                        $amount = $get_transaction->amount;
 
-                    foreach ($product as $product_row) {
-                        $item = json_decode($product_row['product_data'], true);
+                        if ($get_transaction->id != '') {
+                            $this->load->library('deposit');
+                            $this->deposit->topup_deposit($get_transaction);
+                        }
+                    }
 
-                        if ($product_row['variant_id'] != '') {
-                            if (!empty($item['product_variant'])) {
-                                foreach ($item['product_variant'] as $variant) {
-                                    if ($product_row['variant_id'] == $variant['id']) {
-                                        $trans_item[] = array(
-                                            'name'      => $item['name'],
-                                            'unit'      => $variant['name'],
-                                            'price'     => $variant['harga'],
-                                            'qty'       => $product_row['quantity'],
-                                        );
+                    if (substr($transaction_invoice, 0, 2) == 'ST') {
+
+                        $product = $this->conn['main']
+                            ->select('a.*')
+                            ->select("SHA1(CONCAT(variant_id, '" . $this->config->item('encryption_key') . "')) as variant_id")
+                            ->where('transaction_id', $get_transaction->transaction_id)
+                            ->get('mall_transaction_item a')->result_array();
+
+                        foreach ($product as $product_row) {
+                            $item = json_decode($product_row['product_data'], true);
+
+                            if ($product_row['variant_id'] != '') {
+                                if (!empty($item['product_variant'])) {
+                                    foreach ($item['product_variant'] as $variant) {
+                                        if ($product_row['variant_id'] == $variant['id']) {
+                                            $trans_item[] = array(
+                                                'name'      => $item['name'],
+                                                'unit'      => $variant['name'],
+                                                'price'     => $variant['harga'],
+                                                'qty'       => $product_row['quantity'],
+                                            );
+                                        }
                                     }
                                 }
+                            } else {
+                                $trans_item[] = array(
+                                    'name'      => $item['name'],
+                                    'unit'      => $item['price_unit'],
+                                    'price'     => $item['price_selling'],
+                                    'qty'       => $product_row['quantity'],
+                                );
                             }
-                        } else {
-                            $trans_item[] = array(
-                                'name'      => $item['name'],
-                                'unit'      => $item['price_unit'],
-                                'price'     => $item['price_selling'],
-                                'qty'       => $product_row['quantity'],
-                            );
+
+
+                            // update realtime database
+                            if ($get_transaction->service_type != 'ecommerce') {
+                                $this->insert_realtime_database($get_transaction->order_id, 'Pesanan sudah dijadwalkan');
+                            }
+
+                            $update_status_order = $this->conn['main']
+                                ->set(array('status_order' => 'confirm'))
+                                ->where('order_id', $get_transaction->id)
+                                ->where('mitra_id', $get_transaction->merchant_id)
+                                ->update('order_to_mitra');
+
+                            // update merchant_id di mall_order
+                            $update_merchant_id = $this->conn['main']
+                                ->set(array('merchant_id' => $get_transaction->merchant_id, 'transaction_status_id' => 8))
+                                ->where('order_id', $get_transaction->id)
+                                ->update('mall_transaction');
+
+                            //send push notification order to mitra
+                            $this->curl->push($get_transaction->merchant_id, 'Orderan ' . $transaction_invoice . ' telah dibayar', 'Orderanmu siap di lanjutkan!', 'order_pending');
+
+
+                            $user_email = $get_transaction->email;
+                            $order = $get_transaction;
+                            $order_item = $trans_item;
+
+                            $this->send_email_payment_success($order, $order_item, $user_email);
+                            $this->send_email_payment_success_image($user_email);
                         }
-
-                        // update realtime database
-                        if ($get_transaction->service_type != 'ecommerce') {
-                            $this->insert_realtime_database($get_transaction->order_id, 'Pesanan sudah dijadwalkan');
-                        }
-
-                        $update_status_order = $this->conn['main']
-                            ->set(array('status_order' => 'confirm'))
-                            ->where('order_id', $get_transaction->id)
-                            ->where('mitra_id', $get_transaction->merchant_id)
-                            ->update('order_to_mitra');
-
-                        // update merchant_id di mall_order
-                        $update_merchant_id = $this->conn['main']
-                            ->set(array('merchant_id' => $get_transaction->merchant_id, 'transaction_status_id' => 8))
-                            ->where('order_id', $get_transaction->id)
-                            ->update('mall_transaction');
-
-                        //send push notification order to mitra
-                        $this->curl->push($get_transaction->merchant_id, 'Orderan ' . $transaction_invoice . ' telah dibayar', 'Orderanmu siap di lanjutkan!', 'order_pending');
-
-
-                        $user_email = $get_transaction->email;
-                        $order = $get_transaction;
-                        $order_item = $trans_item;
-
-                        $this->send_email_payment_success($order, $order_item, $user_email);
-                        $this->send_email_payment_success_image($user_email);
                     }
                 }
             }
