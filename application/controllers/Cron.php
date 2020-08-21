306be06f2d4285239f29ed6e98f4978e32ecab46
simaung <ddhr00@gmail.com>
2020-08-21 21:03:23 +0700

    change url mutasi bank

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -37,7 +37,7 @@ class Cron extends CI_Controller
         $this->conn['main'] = $this->load->database('default', TRUE);
 
         $this->load->model('payment_model');
-        $link_url = 'https://bank.appsku.net/api/getMutasi/NGNKbFhSRnZNNmF1L2tHdmhJckNVUT09/';
+        $link_url = 'http://bank.sembilankita.com/api/getMutasi/NGNKbFhSRnZNNmF1L2tHdmhJckNVUT09/';
 
         $get_payment_transfer = $this->payment_model->get_payment_transfer(array('status' => 'pending', 'date' => date('Y-m-d')));
         if ($get_payment_transfer) {
