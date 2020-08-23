29519a674e9bf3cbae0ebabe255ab3415d0e996e
simaung <ddhr00@gmail.com>
2020-08-23 15:37:33 +0700

    change less deposit amount for cod if discount existing

--- application/libraries/Deposit.php
+++ application/libraries/Deposit.php
@@ -62,7 +62,7 @@ class Deposit
         );
         $get_persentase_komisi = $this->CI->order_model->getValue('value', 'global_setting', $where);
 
-        $komisi = $data_order->price * (100 - $get_persentase_komisi) / 100;
+        $komisi = ($data_order->price - $data_order->discount) * (100 - $get_persentase_komisi) / 100;
 
         // insert deposit history to mitra
         $data = array(
@@ -73,7 +73,7 @@ class Deposit
             'payment_type'              => 'kredit',
             'payment_referensi'         => $data_order->invoice_code,
             'payment_status'            => 'ok',
-            'payment_message'           => "Pengurangan saldo " . (100 - $get_persentase_komisi) . "%  dari transaksi tunai sebesar Rp. " . number_format($data_order->price, 2, ',', '.')
+            'payment_message'           => "Pengurangan saldo " . (100 - $get_persentase_komisi) . "%  dari transaksi tunai sebesar Rp. " . number_format($data_order->price - $data_order->discount, 2, ',', '.')
         );
 
         $save = $this->CI->order_model->save($data, 'deposit_history');
@@ -84,6 +84,44 @@ class Deposit
 
         $this->CI->order_model->update_data(array('partner_id' => $data_order->mitra_id), $data_update, 'user_partner');
     }
+
+    /*
+    public function add_deposit_cashback_diskon($data_order, $percent_discount)
+    {
+        $this->CI->load->model('order_model');
+        $cond = array(
+            'partner_id' => $data_order->mitra_id,
+        );
+        $get_current_deposit = $this->CI->order_model->getValue('current_deposit', 'user_partner', $cond);
+
+        $where = array(
+            'group' => 'deposit',
+            'name' => 'komisi-mitra',
+        );
+
+        $komisi = $data_order->price * $percent_discount / 100;
+
+        // insert deposit history to mitra
+        $data = array(
+            'partner_id'                => $data_order->mitra_id,
+            'payment_date'              => date('Y-m-d H:i:s'),
+            'payment_amount'            => $komisi,
+            'payment_last_deposit'      => $get_current_deposit,
+            'payment_type'              => 'debet',
+            'payment_referensi'         => $data_order->invoice_code,
+            'payment_status'            => 'ok',
+            'payment_message'           => "Cashback " . ($percent_discount) . "% dari transaksi tunai dengan promo diskon sebesar Rp. " . number_format($data_order->price, 2, ',', '.')
+        );
+
+        $save = $this->CI->order_model->save($data, 'deposit_history');
+
+        $data_update = array(
+            'current_deposit'   => $get_current_deposit + $komisi
+        );
+
+        $this->CI->order_model->update_data(array('partner_id' => $data_order->mitra_id), $data_update, 'user_partner');
+    }
+    */
 
     public function topup_deposit($data_topup)
     {
