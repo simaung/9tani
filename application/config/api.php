33b45ca86db6f771ea3c4d143df84ea141aa502e
simaung <ddhr00@gmail.com>
2020-10-08 12:53:09 +0700

    api callback flip

--- application/config/api.php
+++ application/config/api.php
@@ -10,6 +10,24 @@ $config['api_duitku']['prod']['code'] = 'D2708';
 $config['api_duitku']['prod']['key'] = 'ed28ddd23a32a851ff149eefcf42ccc8';
 $config['api_duitku']['prod']['url'] = 'https://passport.duitku.com/webapi/api/merchant/';
 
+$config['api_midtrans']['dev']['id'] = 'G051329030';
+$config['api_midtrans']['dev']['client'] = 'SB-Mid-client-Dupvl-PooCuqYPgd';
+$config['api_midtrans']['dev']['server'] = 'SB-Mid-server-PT94kelDw1MHFiBV_ziQyzuu';
+$config['api_midtrans']['dev']['url'] = 'https://api.sandbox.midtrans.com/v2/charge';
+
+$config['api_midtrans']['prod']['id'] = 'G051329030';
+$config['api_midtrans']['prod']['client'] = 'Mid-client-khzxOABQ63XKssmv';
+$config['api_midtrans']['prod']['server'] = 'Mid-server-ZiSY4x1nAMmd18ki5SgoYgj3';
+$config['api_midtrans']['prod']['url'] = 'https://api.midtrans.com/v2/charge';
+
+$config['api_bigflip']['prod']['token'] = '$2y$13$oRQ71dKFupLwlLJF6KwgcuvodgjRUVFQ18QnW491o28yULNtbp5iO';
+$config['api_bigflip']['prod']['server'] = 'JDJ5JDEzJGlmd1hGbGVOTXNXU0N4bENGSmt4Q3VMWUx4L0puL0VsMnptQTBOeEV1RVFXYmxEeXczTVdP';
+$config['api_bigflip']['prod']['url'] = 'https://big.flip.id/api/v2/';
+
+$config['api_bigflip']['dev']['token'] = '$2y$13$TTECOYM/zUkD.9/2/QI6fuCRuF9/ipbdhgadOcGjqjtKKhkEzFKQC';
+$config['api_bigflip']['dev']['server'] = 'JDJ5JDEzJGw4WXhxaW9kR1lnUVUvcTdjcjhGR09RMEZOWlFQYm1DcW1SREtoS0J0Rks4TVhDSEtQWVg2';
+$config['api_bigflip']['dev']['url'] = 'https://sandbox.flip.id/api/v2';
+
 // Shipping API
 $config['api_shipping']['url'] = 'https://pro.rajaongkir.com/api/';
 $config['api_shipping']['key'] = 'd9ad2f0889d6523f4e3fcfa36e1bb1a5';
---
+++ application/controllers/Callback.php
@@ -0,0 +1,50 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Callback extends Base_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Connecting to database
+        $this->conn['main'] = $this->load->database('default', TRUE);
+        $this->conn['log']  = $this->load->database('log', TRUE);
+
+        $this->data['api_bigflip'] = $this->config->item('api_bigflip');
+        $this->data['api_bigflip'] = $this->data['api_bigflip'][$this->config->item('payment_env')];
+
+        $firebase = $this->firebase->init();
+        $this->db = $firebase->getDatabase();
+    }
+
+    function flip()
+    {
+        $data = isset($_POST['data']) ? $_POST['data'] : null;
+        $token = isset($_POST['token']) ? $_POST['token'] : null;
+
+        if ($token === $this->data['api_bigflip']['token']) {
+            $data_payment = array(
+                'action'    => 'payment',
+                'post'      => $data,
+                'result'    => $token
+            );
+            $this->conn['log']->insert('log_payment', $data_payment);
+
+            $decoded_data = json_decode($data);
+
+            if ($decoded_data->status == "DONE") {
+                $update_data = $this->conn['main']
+                    ->set(array('payment_status' => 'paid', 'payment_data' => $data))
+                    ->where('payment_status', 'pending')
+                    ->where('id_vendor', $decoded_data->id)
+                    ->update('withdraw_request');
+
+                if ($update_data) {
+                    $this->conn['main']->query("update user_partner set current_deposit = current_deposit - " . $decoded_data->amount . " where partner_id = (select user_id from withdraw_request where id_vendor = " . $decoded_data->id . ")");
+                }
+            }
+        }
+    }
+}
