dcc3863f255e55df26a617e84cf7cc37bd10b453
simaung <ddhr00@gmail.com>
2020-10-15 11:02:28 +0700

    api verification by sms

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -176,6 +176,49 @@ class Customer extends Base_Controller
         $this->print_output();
     }
 
+    public function send_otp()
+    {
+        if ($this->method == 'POST') {
+            $request_data = $this->request['body'];
+
+            $this->load->library(array('form_validation'));
+            $this->form_validation->set_data($request_data);
+
+            $rules[] = array('credential', 'trim|required');
+            $rules[] = array('method', 'trim|required');
+            $rules[] = array('type', 'trim|required');
+
+            set_rules($rules);
+
+            if (($this->form_validation->run() == TRUE)) {
+
+                $user_data['activated_code'] = rand(100000, 999999);
+                $this->user_model->update_data(array('mobile_number' => $request_data['credential']), array('created_at_code' => date('Y-m-d H:i:s'), 'activated_code' => $user_data['activated_code']), 'user_partner');
+
+                $this->send->index('sendOtp', $request_data['credential'], $user_data['activated_code'], $request_data['type']);
+
+                if ($request_data['type'] == 'register') {
+                    $ket = 'OTP proses daftar anda';
+                } elseif ($request_data['type'] == 'login') {
+                    $ket = 'OTP proses login anda';
+                } else {
+                    $ket = 'OTP proses verifikasi nomor telepon anda';
+                }
+                $this->set_response('code', 200);
+                $this->set_response('message', 'Kode ' . $ket . ' telah dikirimkan melalui whatsapp ke nomor ' . $request_data['credential']);
+            } else {
+                $this->set_response('code', 400);
+                $this->set_response('message', sprintf($this->language['error_response'], $this->language['response'][400]['title'], validation_errors()));
+                $this->set_response('data', get_rules_error($rules));
+            }
+        } else {
+            $this->set_response('code', 405);
+        }
+
+        $this->print_output();
+    }
+
+
     public function verifikasi_code()
     {
         if ($this->method == 'POST') {
@@ -250,7 +293,7 @@ class Customer extends Base_Controller
         $this->print_output();
     }
 
-    public function login_sms()
+    function verifikasi_code_sms()
     {
         if ($this->method == 'POST') {
             $request_data = $this->request['body'];
@@ -259,101 +302,45 @@ class Customer extends Base_Controller
             $this->form_validation->set_data($request_data);
 
             $rules[] = array('credential', 'trim|required');
-
-            set_rules($rules);
-
-            if (($this->form_validation->run() == TRUE)) {
-                $params = $request_data;
-
-                $get_data = $this->user_model->read($params);
-
-                if (isset($get_data['code']) && ($get_data['code'] == 200)) {
-                    if ($get_data['response']['data'][0]['user_type'] == 'user') {
-                        $token = hash('sha1', time() . $this->config->item('encryption_key'));
-                        $get_data['response']['data'][0]['ecommerce_token'] = $token;
-                        $user_data = $get_data['response']['data'][0];
-                        $this->user_model->update($get_data['response']['data'][0]['partner_id'], array('ecommerce_token' => $token, 'activated_code' => Null));
-
-                        $this->set_response('code', 200);
-                        $this->set_response('response', array(
-                            'data' => $user_data
-                        ));
-                    } else {
-                        $this->set_response('code', 403);
-                    }
-                }
-            } else {
-                $this->set_response('code', 400);
-                $this->set_response('message', sprintf($this->language['error_response'], $this->language['response'][400]['title'], validation_errors()));
-                $this->set_response('data', get_rules_error($rules));
-            }
-        } else {
-            $this->set_response('code', 405);
-        }
-
-        $this->print_output();
-    }
-
-    public function verification_by_sms()
-    {
-        if ($this->method == 'POST') {
-            $request_data = $this->request['body'];
-
-            $this->load->library(array('form_validation'));
-            $this->form_validation->set_data($request_data);
-
-            $rules[] = array('credential', 'trim|required');
-
-            set_rules($rules);
-
-            if (($this->form_validation->run() == TRUE)) {
-                $params = $request_data;
-
-                $this->user_model->update_data(array('mobile_number' => $params['credential']), array('phone_verified' => '1'), 'user_partner');
-                $this->set_response('code', 200);
-                $this->set_response('message', 'Selamat nomor telepon anda telah berhasil di verifikasi');
-            } else {
-                $this->set_response('code', 400);
-                $this->set_response('message', sprintf($this->language['error_response'], $this->language['response'][400]['title'], validation_errors()));
-                $this->set_response('data', get_rules_error($rules));
-            }
-        } else {
-            $this->set_response('code', 405);
-        }
-
-        $this->print_output();
-    }
-
-    public function send_otp()
-    {
-        if ($this->method == 'POST') {
-            $request_data = $this->request['body'];
-
-            $this->load->library(array('form_validation'));
-            $this->form_validation->set_data($request_data);
-
-            $rules[] = array('credential', 'trim|required');
-            $rules[] = array('method', 'trim|required');
             $rules[] = array('type', 'trim|required');
 
             set_rules($rules);
 
             if (($this->form_validation->run() == TRUE)) {
+                $type = $request_data['type'];
+                unset($request_data['type']);
+                $params = $request_data;
 
-                $user_data['activated_code'] = rand(100000, 999999);
-                $this->user_model->update_data(array('mobile_number' => $request_data['credential']), array('created_at_code' => date('Y-m-d H:i:s'), 'activated_code' => $user_data['activated_code']), 'user_partner');
-
-                $this->send->index('sendOtp', $request_data['credential'], $user_data['activated_code'], $request_data['type']);
-
-                if ($request_data['type'] == 'register') {
-                    $ket = 'OTP proses daftar anda';
-                } elseif ($request_data['type'] == 'login') {
-                    $ket = 'OTP proses login anda';
-                } else {
-                    $ket = 'OTP proses verifikasi nomor telepon anda';
+                if ($type == 'register') {
+                    $cek_credential = strpos($params['credential'], '@');
+                    if ($cek_credential) {
+                        $this->user_model->update_data(array('email' => $params['credential']), array('activated_code' => Null, 'customer_activated' => '1', 'phone_verified' => '1'), 'user_partner');
+                    } else {
+                        $this->user_model->update_data(array('mobile_number' => $params['credential']), array('activated_code' => Null, 'customer_activated' => '1', 'phone_verified' => '1'), 'user_partner');
+                    }
+                    $this->set_response('code', 200);
+                    $this->set_response('message', 'Selamat akun anda telah berhasil di verifikasi');
+                } elseif ($type == 'login') {
+                    $get_data = $this->user_model->read($params);
+                    if (isset($get_data['code']) && ($get_data['code'] == 200)) {
+                        if ($get_data['response']['data'][0]['user_type'] == 'user') {
+                            $token = hash('sha1', time() . $this->config->item('encryption_key'));
+                            $get_data['response']['data'][0]['ecommerce_token'] = $token;
+                            $user_data = $get_data['response']['data'][0];
+                            $this->user_model->update($get_data['response']['data'][0]['partner_id'], array('ecommerce_token' => $token, 'activated_code' => Null));
+                            $this->set_response('code', 200);
+                            $this->set_response('response', array(
+                                'data' => $user_data
+                            ));
+                        } else {
+                            $this->set_response('code', 403);
+                        }
+                    }
+                } elseif ($type == 'verifikasi') {
+                    $this->user_model->update_data(array('mobile_number' => $params['credential']), array('phone_verified' => '1'), 'user_partner');
+                    $this->set_response('code', 200);
+                    $this->set_response('message', 'Selamat nomor telepon anda telah berhasil di verifikasi');
                 }
-                $this->set_response('code', 200);
-                $this->set_response('message', 'Kode ' . $ket . ' telah dikirimkan melalui whatsapp ke nomor ' . $request_data['credential']);
             } else {
                 $this->set_response('code', 400);
                 $this->set_response('message', sprintf($this->language['error_response'], $this->language['response'][400]['title'], validation_errors()));
