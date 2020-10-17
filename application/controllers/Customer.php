fcebdfb3c158f46418c52961d35fabf02e7b1e2f
simaung <ddhr00@gmail.com>
2020-10-17 07:13:50 +0700

    set phone verified 1 when login by phone

--- application/controllers/Customer.php
+++ application/controllers/Customer.php
@@ -272,8 +272,9 @@ class Customer extends Base_Controller
                             $get_data['response']['data'][0]['activated_code'] = Null;
                             $user_data = $get_data['response']['data'][0];
 
-                            if ($user_data['phone_verified'] == '0'  && !$cek_credential) {
+                            if ($get_data['response']['data'][0]['phone_verified'] == '0'  && !$cek_credential) {
                                 $this->user_model->update_data(array('mobile_number' => $params['credential']), array('phone_verified' => '1'), 'user_partner');
+                                $user_data['phone_verified'] = '1';
                             }
                             $this->user_model->update($get_data['response']['data'][0]['partner_id'], array('ecommerce_token' => $token, 'activated_code' => Null));
 
@@ -351,13 +352,16 @@ class Customer extends Base_Controller
                         if ($get_data['response']['data'][0]['user_type'] == 'user') {
                             $token = hash('sha1', time() . $this->config->item('encryption_key'));
                             $get_data['response']['data'][0]['ecommerce_token'] = $token;
+                            $get_data['response']['data'][0]['activated_code'] = Null;
+
                             $user_data = $get_data['response']['data'][0];
 
-                            if ($user_data['phone_verified'] == '0') {
+                            if ($user_data['phone_verified'] == '0' && !$cek_credential) {
                                 $this->user_model->update_data(array('mobile_number' => $params['credential']), array('phone_verified' => '1'), 'user_partner');
+                                $user_data['phone_verified'] = '1';
                             }
-
                             $this->user_model->update($get_data['response']['data'][0]['partner_id'], array('ecommerce_token' => $token, 'activated_code' => Null));
+
                             $this->set_response('code', 200);
                             $this->set_response('response', array(
                                 'data' => $user_data
