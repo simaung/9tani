6368f004cf15b62b0a894e3137110af3d9d2e6a1
simaung <ddhr00@gmail.com>
2020-10-19 18:44:04 +0700

    add respon term voucher

--- application/models/Voucher_model.php
+++ application/models/Voucher_model.php
@@ -11,7 +11,7 @@ class Voucher_model extends Base_Model
 
     public function read($params = array(), $action = '')
     {
-        $this->conn['main']->select('name, type_product, name, description, amount, percent,start_periode, end_periode, min_transaksi');
+        $this->conn['main']->select('name, type_product, name, description, term, amount, percent,start_periode, end_periode, min_transaksi');
         $this->conn['main']->where('status_active', '1');
         $this->conn['main']->where('name !=', 'global_discount');
         $this->conn['main']->where('( CASE WHEN start_periode IS NOT NULL THEN NOW() BETWEEN start_periode AND end_periode ELSE start_periode IS NULL END )', null, false);
