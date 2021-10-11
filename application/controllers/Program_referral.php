c06e2773ed4e85577e423d3ddd118fc536d4ad1f
simaung <ddhr00@gmail.com>
2021-10-11 19:04:28 +0700

    api program referral

---
+++ application/controllers/Program_referral.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Program_referral extends Base_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        // Load model
+        $this->load->model(array('base_model'));
+    }
+
+    // Get list
+    public function index()
+    {
+        if ($this->method == 'GET') {
+            // GET DATA
+            $get_data = $this->base_model->getWhere('mstr_referral', array('status_active' => '1'));
+
+            if ($get_data) {
+                foreach ($get_data as $row) {
+                    unset($row->id);
+                    $this->set_response('code', 200);
+                    $this->set_response('response', $get_data);
+                }
+            } else {
+                $this->set_response('code', 404);
+            }
+        } else {
+            $this->set_response('code', 405);
+        }
+
+        $this->print_output();
+    }
+}
