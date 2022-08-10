4e431400987ac7b8e0c1cc664dd5fb96128e0026
Luthfi Fadle Rynaldy <luthfifadle@gmail.com>
2022-08-10 16:53:07 +0700

    Fix get_transaction parameter

--- application/controllers/Payment.php
+++ application/controllers/Payment.php
@@ -1287,16 +1287,23 @@ class Payment extends Base_Controller
                     if ($params_response['result'] == 'success') {
                         $get_transaction->status = $payment_status;
                         $get_transaction->mp_result = $params_response;
-                        
+                        $get_transaction->invoice_code = $get_transaction->refid;
+                        $get_transaction->description = 'Bayar dengan Gopay';
+                        $get_transaction->total_price = $get_transaction->jumlah_dibayar;
+                        $get_transaction->shipping_cost = 0;
+                        $get_transaction->payment_status = $get_transaction->status;
+                        $get_transaction = 0;
+
                         $this->data['message'] = $this->language['payment_finish'];
-                        $this->data['order_detail'] = array(
-                            'invoice_code' => $get_transaction->refid,
-                            'description' => 'Bayar dengan Gopay',
-                            'total_price' => $get_transaction->jumlah_dibayar,
-                            'shipping_cost' => 0,
-                            'payment_status' => $get_transaction->status,
-                            'flag_device' => 0
-                        );
+                        // $this->data['order_detail'] = array(
+                        //     'invoice_code' => $get_transaction->refid,
+                        //     'description' => 'Bayar dengan Gopay',
+                        //     'total_price' => $get_transaction->jumlah_dibayar,
+                        //     'shipping_cost' => 0,
+                        //     'payment_status' => $get_transaction->status,
+                        //     'flag_device' => 0
+                        // );
+                        $this->data['order_detail'] = $get_transaction;
                         $this->data['data_redirect'] = 'merchantOrderId=' . $params_response['order_id'] . '&resultCode=' . $params_response['result'];
     
                         $this->load->view('payment_complete', $this->data);
